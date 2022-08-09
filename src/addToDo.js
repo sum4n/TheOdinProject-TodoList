cf8b8c48ab32f681fc65f94ec9ab450a162e6cf3
Suman Das <sum4ndas@gmail.com>
2022-08-09 20:45:30 +0530

    Move from elements to addToDoForm.js

--- src/addToDo.js
+++ src/addToDo.js
@@ -1,5 +1,6 @@
 import plusIconImg from './images/plus.png';
 import './addToDo.css';
+import formDiv from './addToDoForm';
 
 export default function() {
     const plusIconDiv = document.createElement('div');
@@ -11,50 +12,7 @@ export default function() {
         alert("hello");
     });
 
-    const formDiv = document.createElement('form');
-
-    // first div
-    const firstFormDiv = document.createElement('div');
-
-    const inputTitle = document.createElement('input');
-    inputTitle.setAttribute("type", "text");
-    inputTitle.setAttribute("placeholder", "name");
-
-    const inputDate = document.createElement('input');
-    inputDate.setAttribute("type", "date");
-
-    firstFormDiv.append(inputTitle, inputDate);
-    // first div  ends
-    // second form div
-    const secFormDiv = document.createElement('div');
-
-    const inputPriority = document.createElement('select');
-    inputPriority.setAttribute('name', 'Priority');
-
-    const prioritySet = document.createElement('option');
-    prioritySet.setAttribute('value', 'Set Prioirity');
-    prioritySet.textContent = "Set Priority";
-
-     const priorityLow = document.createElement('option');
-    priorityLow.setAttribute('value', 'low');
-    priorityLow.textContent = "Low";
-
-     const priorityMedium = document.createElement('option');
-    priorityMedium.setAttribute('value', 'medium');
-    priorityMedium.textContent = "Medium";
-
-     const priorityHigh = document.createElement('option');
-    priorityHigh.setAttribute('value', 'high');
-    priorityHigh.textContent = "High";
-
-
-    inputPriority.append(prioritySet, priorityLow, priorityMedium, priorityHigh);
-
-    secFormDiv.append(inputPriority);
-
-    formDiv.append(firstFormDiv, secFormDiv);
-
-    plusIconDiv.appendChild(formDiv);
+    plusIconDiv.appendChild(formDiv());
 
     plusIconDiv.appendChild(plusIcon);
 
---
+++ src/addToDoForm.js
@@ -0,0 +1,46 @@
+export default function() {
+    const formDiv = document.createElement('form');
+
+    // first div
+    const firstFormDiv = document.createElement('div');
+
+    const inputTitle = document.createElement('input');
+    inputTitle.setAttribute("type", "text");
+    inputTitle.setAttribute("placeholder", "name");
+
+    const inputDate = document.createElement('input');
+    inputDate.setAttribute("type", "date");
+
+    firstFormDiv.append(inputTitle, inputDate);
+    // first div  ends
+    // second form div
+    const secFormDiv = document.createElement('div');
+
+    const inputPriority = document.createElement('select');
+    inputPriority.setAttribute('name', 'Priority');
+
+    const prioritySet = document.createElement('option');
+    prioritySet.setAttribute('value', 'Set Prioirity');
+    prioritySet.textContent = "Set Priority";
+
+     const priorityLow = document.createElement('option');
+    priorityLow.setAttribute('value', 'low');
+    priorityLow.textContent = "Low";
+
+     const priorityMedium = document.createElement('option');
+    priorityMedium.setAttribute('value', 'medium');
+    priorityMedium.textContent = "Medium";
+
+     const priorityHigh = document.createElement('option');
+    priorityHigh.setAttribute('value', 'high');
+    priorityHigh.textContent = "High";
+
+
+    inputPriority.append(prioritySet, priorityLow, priorityMedium, priorityHigh);
+
+    secFormDiv.append(inputPriority);
+
+    formDiv.append(firstFormDiv, secFormDiv);
+
+    return formDiv;
+}
